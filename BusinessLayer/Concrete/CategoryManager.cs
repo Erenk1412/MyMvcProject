1ef3c1e92333ded2c9a163d3ab98fee4d013f1b7
EREN KECHAGIA <eren.kehaya@gmail.com>
2021-04-23 23:50:15 +0300

    Mvc Project

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,32 @@
+﻿using DataAccessLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager
+    {
+        GenericRepository<Category> repository = new GenericRepository<Category>();
+
+        public List<Category> GetAll()
+        {
+            return repository.List();
+        }
+
+        public void Add(Category category)
+        {
+            if (category.CategoryName == "" || category.CategoryName.Length <= 3 || category.CategoryDescription == "" || category.CategoryName.Length >= 51)
+            {
+
+            }
+            else
+            {
+                repository.Insert(category);
+            }
+        }
+    }
+}
---
+++ DataAccessLayer/Concrete/Repositories/CategoryRepository.cs
@@ -0,0 +1,37 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.Repositories
+{
+    public class CategoryRepository
+    {
+        Context context = new Context();
+        DbSet<Category> _object;
+        public void Delete(Category category)
+        {
+            _object.Remove(category);
+            context.SaveChanges();
+        }
+
+        public void Insert(Category category)
+        {
+            _object.Add(category);
+            context.SaveChanges();
+        }
+
+        public List<Category> List()
+        {
+            return _object.ToList();
+        }
+
+        public void Update(Category category)
+        {
+            context.SaveChanges();
+        }
+    }
+}
